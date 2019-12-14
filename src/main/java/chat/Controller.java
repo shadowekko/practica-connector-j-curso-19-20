ae37ad59d2902f46c0698ff2c83628cefe215b38
shadowekko <56632277+shadowekko@users.noreply.github.com>
2019-12-14 17:49:39 +0100

    He añadido otros dato de conexion para hacerlo desde localhost

--- src/main/java/chat/Controller.java
+++ src/main/java/chat/Controller.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 public class Controller {
 
-    // @TODO: Sustituya xxxx por los parámetros de su conexión
+ /**   // @TODO: Sustituya xxxx por los parámetros de su conexión
     private final String DB_SERVER = "bhcwv8kdmdkrydy512sa-mysql.services.clever-cloud.com";
     private final int DB_PORT = 3306;
     private final String DB_NAME = "bhcwv8kdmdkrydy512sa";
@@ -22,7 +22,14 @@ public class Controller {
     private final String DB_PASS = "xviv5csnZmexnxpON4LL";
 
     private Connection conn;
+*/
+    private final String DB_SERVER = "localhost";
+    private final int DB_PORT = 3306;
+    private final String DB_NAME = "chat";
+    private final String DB_USER = "root";
+    private final String DB_PASS = "xve$8520";
 
+    private Connection conn;
     /**
      * Crea un nuevo controlador
      */
